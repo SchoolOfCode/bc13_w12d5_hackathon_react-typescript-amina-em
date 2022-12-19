b366b1cc21a07ecc3a4e1425a61d27a2143cf919
emilykpritchard <emilykpritchard@me.com>
2022-12-19 12:31:48 +0000

    WhatIsWithTheDate

--- my-app/src/component/fiveDayWeather/fiveDayCard.tsx
+++ my-app/src/component/fiveDayWeather/fiveDayCard.tsx
@@ -1,13 +1,17 @@
+import {useState} from "react";
+
 type FiveDayCardProps = {
-    date: number
+    date: any
     description: string
     icon: any
     temperature: number
 }
 
 export default function FiveDayCard(props: FiveDayCardProps){
-    return <div>
-        <h3>{props.date}</h3>
+
+        let newDate = props.date.toDateString();
+        return <div className="fiveDaySingleCard">
+        <h3>{newDate}</h3>
         <img src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`} alt={"icon"}/>
         <p>{props.temperature}</p>
         <p>{props.description}</p> 
