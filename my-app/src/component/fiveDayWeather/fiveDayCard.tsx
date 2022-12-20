703dc8d5e767e31da5d2ee575f06402ae8352206
emilykpritchard <emilykpritchard@me.com>
2022-12-20 22:19:59 +0000

    Fixing timestamp

--- my-app/src/component/fiveDayWeather/fiveDayCard.tsx
+++ my-app/src/component/fiveDayWeather/fiveDayCard.tsx
@@ -1,19 +1,22 @@
-import {useState} from "react";
-
 type FiveDayCardProps = {
-    date: any
-    description: string
-    icon: any
-    temperature: number
-}
+  date: number;
+  description: string;
+  icon: any;
+  temperature: number;
+};
 
-export default function FiveDayCard(props: FiveDayCardProps){
-
-        let newDate = props.date.toDateString();
-        return <div className="fiveDaySingleCard">
-        <h3>{newDate}</h3>
-        <img src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`} alt={"icon"}/>
-        <p>{props.temperature}</p>
-        <p>{props.description}</p> 
+export default function FiveDayCard(props: FiveDayCardProps) {
+  let myDate = new Date(props.date * 1000);
+  const finalDate = myDate.toLocaleDateString();
+  return (
+    <div className="fiveDaySingleCard">
+      <h3>{finalDate}</h3>
+      <img
+        src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`}
+        alt={"icon"}
+      />
+      <p>{props.temperature}</p>
+      <p>{props.description}</p>
     </div>
+  );
 }
