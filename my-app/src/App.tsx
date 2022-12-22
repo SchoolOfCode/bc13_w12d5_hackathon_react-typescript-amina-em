a0618a09b7e571fe2eea7ad52af0d26a4a7e130b
edmundsamina <edmundsamina@gmail.com>
2022-12-22 12:30:50 +0000

    changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,13 +3,13 @@ import {useState, useEffect} from "react";
 import WeatherWidget from './component/weather/weather';
 import Search from './component/search/search';
 import Button from './component/button/Button';
-import newImages from './data/data';
-import FiveDayWeather from './component/fiveDayWeather/fiveDay'
+import {newImageMap} from './data/data';
+
 
 function App() {
 
   const [tempC, setTempC] = useState(0);
-  const [location, setLocation] = useState({City: "", Country:""});
+  const [locationForm, setLocationForm] = useState({City: "", Country:""});
   const [locationOnClick, setLocationOnClick]= useState({City: "london", Country:"uk"});
   const [icon, setIcon] = useState("");
   const [cityTag, setCityTag] = useState("");
@@ -27,9 +27,9 @@ function App() {
         (data.main.temp - 273.15).toFixed(1)
       );
       const myIcon = data.weather[0].icon
-      if(myIcon === '01d' || myIcon === '01n'){
-        setIcon(newImages.clearSky01dn);
-      }
+     
+     setIcon(newImageMap[myIcon]);
+      
      
       setTempC(temp);
       setCityTag(data.name);
@@ -43,54 +43,36 @@ function App() {
   function getWeatherByLocation(): void {
     async function getData() {
       const result = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${location.City},${location.Country}&APPID=fc45f1c741819c4bf127ffd04b8ee142`
+        `https://api.openweathermap.org/data/2.5/weather?q=${locationForm.City},${locationForm.Country}&APPID=fc45f1c741819c4bf127ffd04b8ee142`
       );
       const data = await result.json();
       const temp: number | undefined = Number(
         (data.main.temp - 273.15).toFixed(1)
       );
       const myIcon = data.weather[0].icon
-      
-      if(myIcon === '01d' || myIcon === '01n'){
-        setIcon(newImages.clearSky01dn);
-      } else if (myIcon === '02d' || myIcon === '02n'){
-        setIcon(newImages.fewClouds02dn);
-      } else if (myIcon === '03d' || myIcon === '03n' || myIcon === '04d' || myIcon === '04n'){
-        setIcon(newImages.scatteredClouds03dn);
-      } else if (myIcon === '09d' || myIcon === '09n'){
-        setIcon(newImages.showerRain09dn);
-      } else if (myIcon === '10d' || myIcon === '10n'){
-        setIcon(newImages.rain10dn);
-      } else if (myIcon === '11d' || myIcon === '11n'){
-        setIcon(newImages.thunderstorm11dn);
-      } else if (myIcon === '13d' || myIcon === '13n'){
-        setIcon(newImages.snow13d);
-      } else if (myIcon === '50d' || myIcon === '50n'){
-        setIcon(newImages.mist50d);
-      }
-      
+      setIcon(newImageMap[myIcon]);
      
       setTempC(temp);
       setCityTag(data.name);
-      setLocationOnClick(location);
+      setLocationOnClick(locationForm);
     }
     getData();
   };
   
   function onChangeCity(e: React.ChangeEvent<HTMLInputElement>){
-    setLocation({...location, City:e.target.value})
+    setLocationForm({...locationForm, City:e.target.value})
   }
   
   return (
     <div className="App">
       <div className="findCity">
       <Search placeholder="Search City" onChange={onChangeCity}/>
-     <Button onClick={getWeatherByLocation}></Button>
+      <Button onClick={getWeatherByLocation}></Button>
      </div>
-     <WeatherWidget city = {cityTag} icon={icon} tempC={tempC} location={location} humidity={humidity} windspeed={windspeed} weatherDescription={weatherDescription}/>
+     <WeatherWidget locationOnClick={locationOnClick} city = {cityTag} icon={icon} tempC={tempC} location={locationForm} humidity={humidity} windspeed={windspeed} weatherDescription={weatherDescription}/>
     
     <div>
-      <FiveDayWeather location={locationOnClick}/>
+
     </div>
     </div>
   );
